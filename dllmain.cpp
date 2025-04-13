95ee478350f1a931bc138863bd4118b68c49e794
Clippy95 <qpzezoqp15@gmail.com>
2025-04-13 20:18:41 +0300

    big changes

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,16 +8,47 @@
 
 #include <chrono> 
 
+void patchJmp(void* addr, void* func) {
+	DWORD oldProtect;
 
-static float EditedCameraPosition = 0.f;
-static std::chrono::steady_clock::time_point TimerStart; // Start time of the timer
-static const std::chrono::seconds InterpolationDuration(2); // Interpolation duration (2 seconds)
-static bool IsTimerActive = false; // Flag to indicate if the timer is active
+	VirtualProtect(addr, 5, PAGE_EXECUTE_READWRITE, &oldProtect);
+	*(uint8_t*)addr = 0xE9;
+	*(uint32_t*)((uint8_t*)addr + 1) = (uint32_t)func - (uint32_t)addr - 5;
+	VirtualProtect(addr, 5, oldProtect, &oldProtect);
+}
 
-static int onfootIndex = 2; // Default index in the array (0 position)
-static int inVehicleIndex = 2;
-static int fineAimIndex = 2;
-static const std::vector<float> cameraAdjustments = { -2.f, -1.f, 0.f, 1.f, 2.f };
+
+bool wasPressedThisFrame[256];
+bool lastFrameStates[256];
+void UpdateKeys()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		bool thisFrameState = GetKeyState(i) < 0;
+		wasPressedThisFrame[i] = thisFrameState && !lastFrameStates[i];
+		lastFrameStates[i] = thisFrameState;
+	}
+}
+
+inline float lerp(float a, float b, float t) {
+	return a + t * (b - a);
+}
+
+__declspec(noinline) bool IsKeyPressed(unsigned char Key, bool Hold) // USE THIS FROM NOW ON
+{
+	if (*(uint8_t*)0x252A406)
+	{
+		if (Hold)
+		{
+			return lastFrameStates[Key];
+		}
+		else
+		{
+			return wasPressedThisFrame[Key];
+		}
+	}
+	return false;
+}
 
 float getDeltaTime() {
     static DWORD lastTime = GetTickCount64();
@@ -27,121 +58,76 @@ float getDeltaTime() {
     return elapsedMillis / 1000.0f;
 }
 
-bool inline isplayerstatus(uint8_t status) {
-    return (*(BYTE*)0x00E9A5BC == status);
-}
-
-uint8_t inline static playerstatus() {
-    return *(BYTE*)0x00E9A5BC;
-}
-
-enum playerstatus {
-    vehicle = 3,
-    boat = 5,
-    helicopter = 6,
-    plane = 9,
-    sniper = 9,
-    fineaimcrouch = 16,
-    fineaim = 17,
-};
-
-float OriginalCameraPosition;
-
-bool IsInterpolationActive() {
-    if (!IsTimerActive)
-        return false;
-
-    auto elapsed = std::chrono::steady_clock::now() - TimerStart;
-    if (elapsed >= InterpolationDuration) {
-        IsTimerActive = false;
-        return false;
-    }
-    return true;
-}
-
-void ActivateInterpolationTimer() {
-    TimerStart = std::chrono::steady_clock::now();
-    IsTimerActive = true;
-}
-
-void EditCameraPosition(float newzoom) {
-    OriginalCameraPosition = *(float*)0x00E9A654;
-
-    if (OriginalCameraPosition <= 1.f) {
-        EditedCameraPosition = OriginalCameraPosition;
-        return;
-    }
-
-    if (IsInterpolationActive()) {
-        float targetPosition = OriginalCameraPosition + newzoom;
-        float smoothingfactor = 5.f;
-
-        if (isplayerstatus(playerstatus::fineaimcrouch) || isplayerstatus(playerstatus::fineaim)) {
-            if (newzoom != 0)
-                targetPosition = OriginalCameraPosition + (newzoom / 5.f);
-            smoothingfactor *= 2.f;
-        }
-
-        EditedCameraPosition += (targetPosition - EditedCameraPosition) * (getDeltaTime() * smoothingfactor);
-    }
-    else {
-        EditedCameraPosition = OriginalCameraPosition + newzoom;
-    }
-}
-
-void HandleKeyPress() {
-    static bool keyPressed = false;
-    uint8_t status = playerstatus();
-    int* activeIndex = nullptr;
-
-    switch (status) {
-    case playerstatus::fineaimcrouch:
-    case playerstatus::fineaim:
-        activeIndex = &fineAimIndex;
-        break;
-    case playerstatus::vehicle:
-    case playerstatus::boat:
-    case playerstatus::helicopter:
-    case playerstatus::plane:
-        activeIndex = &inVehicleIndex;
-        break;
-    default:
-        activeIndex = &onfootIndex;
-        break;
-    }
-
-    if (*(uint8_t*)0x252A406) {
-        if (GetAsyncKeyState('B') & 0x8000) {
-            if (!keyPressed) {
-                keyPressed = true;
-
-                *activeIndex = (*activeIndex + 1) % cameraAdjustments.size();
-                *(bool*)(0x252740E) = 1;
-
-                ActivateInterpolationTimer();
-            }
-        }
-        else {
-            keyPressed = false;
-        }
-    }
-
-    EditCameraPosition(cameraAdjustments[*activeIndex]);
-}
-
+float EditedZoomMod = 0.f;
+float zoom_values[] = { -2.f, -1.f, 0.f, 1.f, 2.f };
+BYTE onfootIndex = 2;
+BYTE inVehicleIndex = 2;
+BYTE fineAimIndex = 2;
 void PerformCustomMemoryCopy() {
-    HandleKeyPress();
-    uint8_t* src = reinterpret_cast<uint8_t*>(0x00E9A638);
-    uint8_t* dest = reinterpret_cast<uint8_t*>(0x00E9A60C);
-    size_t size = 0x2C;
+	UpdateKeys();
+		enum status : BYTE {
+			vehicle = 3,
+			boat = 5,
+			helicopter = 6,
+			plane = 9,
+			sniper = 9,
+			fineaimcrouch = 16,
+			fineaim = 17,
+		};
+		BYTE* activeIndex = &onfootIndex;
+		status player_status = *(status*)0x00E9A5BC;
+		switch (player_status) {
+		case vehicle:
+		case boat:
+		case helicopter:
+		case plane:
+			activeIndex = &inVehicleIndex;
+			break;
+		case fineaimcrouch:
+		case fineaim:
+			activeIndex = &fineAimIndex;
+			break;
+		default:
+			activeIndex = &onfootIndex;
+			break;
+		}
+		float target = zoom_values[*activeIndex];
+		switch (player_status) {
+		case helicopter:
+		case plane:
+			target *= 4.f;
+			break;
+		case fineaimcrouch:
+		case fineaim:
+			if(target > 0.f)
+			target *= 0.35f;
+			break;
+		default:
+			break;
+		}
+		float speed = 5.0f;
 
-    for (size_t i = 0; i < size; ++i) {
-        if (reinterpret_cast<uintptr_t>(src) == 0x00E9A654 && OriginalCameraPosition > 1.f) {
-            src = reinterpret_cast<uint8_t*>(&EditedCameraPosition);
-        }
-        dest[i] = *src++;
-    }
-}
+		float t = speed * getDeltaTime();
+		if (t > 1.0f) t = 1.0f;
+
+		// Update EditedZoomMod towards the target value
+		EditedZoomMod = lerp(EditedZoomMod, target, t);
+		if (IsKeyPressed('B', false)) {
+			*activeIndex = (*activeIndex + 4) % 5;
+		}
+		float* src = reinterpret_cast<float*>(0x00E9A638);
+		float* dest = reinterpret_cast<float*>(0x00E9A60C);
+		size_t size = 0x2C / 4;
+
+		for (size_t i = 0; i < size; ++i) {
+			if (reinterpret_cast<uintptr_t>(src) == 0x00E9A654) {
+				dest[i] = (*(float*)0x00E9A654) + EditedZoomMod;
+				*src++;
+			}
+			else
+				dest[i] = *src++;
+		}
+	}
 
 void __declspec(naked) HookedRepMovsd() {
     __asm {
@@ -149,9 +135,8 @@ void __declspec(naked) HookedRepMovsd() {
         pushfd
     }
 
-    __asm {
-        call PerformCustomMemoryCopy
-    }
+	PerformCustomMemoryCopy();
+    
 
     __asm {
         popfd
@@ -162,22 +147,8 @@ void __declspec(naked) HookedRepMovsd() {
     }
 }
 
-typedef void(__stdcall* RepMovsdFunc)();
-RepMovsdFunc originalRepMovsd = nullptr;
-
 void setupHook() {
-    if (MH_Initialize() != MH_OK) {
-        MessageBoxW(NULL, L"FAILED TO INITIALIZE", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-    if (MH_CreateHook((LPVOID)0x0049A4D0, &HookedRepMovsd, (LPVOID*)&originalRepMovsd) != MH_OK) {
-        MessageBoxW(NULL, L"FAILED TO HOOK", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
-    if (MH_EnableHook(MH_ALL_HOOKS) != MH_OK) {
-        MessageBoxW(NULL, L"FAILED TO ENABLE", L"Error", MB_OK | MB_ICONERROR);
-        return;
-    }
+	patchJmp((void*)0x0049A4CB, HookedRepMovsd);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
